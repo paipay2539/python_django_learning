c6f76de14d66edd8fc86c98a8ff5814fd85eaa9d
paidev2539 <paidev2539@gmail.com>
2021-09-12 21:20:16 +0700

    save url + app learn

--- django_learning_project/app_01/views.py
+++ django_learning_project/app_01/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+def hello1(request):
+    return HttpResponse('Hello World1')
+
+def hello2_1(request, id):
+    text = "Hello World2_1 " + str(id)
+    return HttpResponse(text)
+
+def hello2_2(request, year, slug):
+    text = "Hello World2_2 " + str(year)+ " " + str(slug)
+    return HttpResponse(text)
+
+def hello3(request, year, slug):
+    text = "Hello World3 " + str(year)+ " " + str(slug)
+    return HttpResponse(text)
--- django_learning_project/django_learning_project/urls.py
+++ django_learning_project/django_learning_project/urls.py
@@ -14,8 +14,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from app_01 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', views.hello1, name='hello1'),
+    path('hello/<int:id>/abc-def/', views.hello2_1, name='hello2_1'),
+    re_path(r'hello/(?P<year>[0-9]{4})/(?P<slug>[\w-]+)/$', views.hello2_2, name='hello2_2'),
+    re_path(r'hello/(?P<year>[0-9]{4})/(?P<slug>[\w-]+)', views.hello3, name='hello3'),
+
+    # + : free, {4} : only 4 char
+    # pattern : (?P<variable_name> )
+    # [0-9] : digit, [\w-] : word that join by '-'
+    # http://localhost:8000/hello_re/1234/abc-def
+    # last / is effect
+    # if same condition it will use first match condition path(..) on this file
+    # 2_1 is same condition with 2_2 it will use 2_1
+    # re_path ..) : endurl, ..)/$ : endurl/
+    # path <int:id> : 123, <int:id>/ : 123/
 ]
